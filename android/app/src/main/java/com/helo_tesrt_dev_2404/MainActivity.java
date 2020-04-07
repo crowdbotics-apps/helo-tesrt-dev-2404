916d2f8026bd5a8beb2913f2bb55afc2ab0d897b
Crowdbotics <team@crowdbotics.com>
2020-04-07 10:04:52 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/helo_tesrt_dev_2404/MainActivity.java
@@ -0,0 +1,15 @@
+package com.helo_tesrt_dev_2404;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "helo_tesrt_dev_2404";
+    }
+}
